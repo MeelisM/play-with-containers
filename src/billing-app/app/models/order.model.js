96afd65c6232fa1073874ab410a487ceeb6aebed
Meelis Mumm <mumm.meelis@gmail.com>
2025-02-24 16:11:44 +0200

    add createdAt and updatedAt fields to billing model

--- src/billing-app/app/models/order.model.js
+++ src/billing-app/app/models/order.model.js
@@ -1,23 +1,40 @@
 const defineOrder = (sequelize, Sequelize) => {
-  const Order = sequelize.define("order", {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  const Order = sequelize.define(
+    "order",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number_of_items: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      total_amount: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
     },
-    user_id: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    number_of_items: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    total_amount: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: true,
+      tableName: "orders",
+    }
+  );
 
   return Order;
 };
